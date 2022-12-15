6930586f3f4f796aa0f2ba9c24f03871efc1df1e
Hugo <hugo.pratasdesousa@gmail.com>
2022-12-15 13:06:19 +0000

    continuação, likes

--- src/app/pages/video-page/video-page.component.ts
+++ src/app/pages/video-page/video-page.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute} from '@angular/router';
 import {UpTubeServiceService} from "../../services/up-tube-service.service";
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 import {faFlag} from "@fortawesome/free-solid-svg-icons";
+import {faThumbsUp} from "@fortawesome/free-regular-svg-icons";
+import {faThumbsDown} from "@fortawesome/free-regular-svg-icons";
+
 
 @Component({
   selector: 'app-video-page',
@@ -16,6 +19,8 @@ export class VideoPageComponent implements OnInit {
   data: any;
   user: any;
   flag = faFlag;
+  thumbsUp = faThumbsUp
+  thumbsDown = faThumbsDown
 
   constructor(private route: ActivatedRoute, private _service: UpTubeServiceService, private sanitizer: DomSanitizer) {
   }
--- src/app/services/up-tube-service.service.ts
+++ src/app/services/up-tube-service.service.ts
@@ -12,36 +12,43 @@ export class UpTubeServiceService {
       "title": "Finlandia",
       "tags": ["natureza", "países", "beleza", "paisagem"],
       "video": "https://www.youtube.com/watch?v=F5zg_af9b8c",
-      "user_id":1
+      "user_id": 1,
+      "date": new Date(2022, 3, 17, 12, 45, 37, 10)
     },
     {
       "id": 2,
       "title": "I Can Only Count to FOUR",
       "tags": ["rock", "parody"],
       "video": "https://www.youtube.com/watch?v=u8ccGjar4Es",
-      "user_id":2
+      "user_id": 2,
+      "date": new Date(2022, 3, 17, 12, 45, 37, 10)
+
     },
     {
       "id": 3,
       "title": "Norway",
       "tags": ["natureza", "países", "beleza", "paisagem"],
       "video": "https://youtube.com/watch?v=5kJFSSP53mU",
-      "user_id":1
+      "user_id": 1,
+      "date": new Date(2022, 3, 17, 12, 45, 37, 10)
     }
   ]
 
-  user=[
+  user = [
     {
       "id": 1,
-      "logo": "../../assets/images/user_logos/unnamed.jpg"
+      "logo": "../../assets/images/user_logos/unnamed.jpg",
+      "name": "Paisagens.pt"
     },
     {
-      "id":2,
-      "logo": "../../assets/images/user_logos/índice.jpg"
+      "id": 2,
+      "logo": "../../assets/images/user_logos/índice.jpg",
+      "name": "Swedish House Parody"
+
     }
   ]
 
-  tags=["pop", "rock", "metal", "natureza", "musica", "dança"]
+  tags = ["pop", "rock", "metal", "natureza", "musica", "dança"]
 
   getVideo(id: number) {
     return this.videos.find(obj => obj.id == id);
@@ -51,7 +58,7 @@ export class UpTubeServiceService {
     return this.tags
   }
 
-  getUser(id: number){
+  getUser(id: number) {
     return this.user.find(obj => obj.id == id)
   }
 
