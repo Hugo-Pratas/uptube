5a9d8f910a168348df9b2fdad52282e648f40b44
Hugo de Sousa <hugo.pratasdesousa@gmail.com>
2022-12-28 19:52:54 +0000

    toques video card

--- src/app/components/video-card/video-card.component.ts
+++ src/app/components/video-card/video-card.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UpTubeServiceService} from "../../services/up-tube-service.service";
+import {faShareAlt} from "@fortawesome/free-solid-svg-icons";
+import {faBookmark} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
   selector: 'app-video-card',
@@ -11,6 +13,8 @@ export class VideoCardComponent implements OnInit {
   apiRoute = ""
   @Input() video_data: any;
   processPage = false
+  faShareSquare=faShareAlt
+  bookmark=faBookmark
 
 
   constructor(private _service: UpTubeServiceService) {
@@ -21,7 +25,21 @@ export class VideoCardComponent implements OnInit {
     this._service.getUser(this.video_data.channel).subscribe(d => {
       this.user = d
       this.user = this.user[0] //api retorna array...
+      this.bookmark=this._service.icone_favorito(parseInt(this.video_data.id))
       this.processPage = true
     })
   }
+
+  toggleFavorito(id_video: number) {
+    this._service.toggleFavorito(id_video)
+    this.bookmark = this._service.icone_favorito(id_video)
+  }
+
+  myFunction() {
+    var popup = document.getElementById(this.video_data.id);
+    navigator.clipboard.writeText(document.URL.replace("homepage", "video/") + this.video_data.id);
+    // @ts-ignore
+    popup.classList.toggle("show");
+  }
+
 }
