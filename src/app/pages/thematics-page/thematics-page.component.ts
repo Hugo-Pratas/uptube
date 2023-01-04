769bb67d0fc494b315183a17e341d9b08931dba1
Hugo <hugo.pratasdesousa@gmail.com>
2023-01-04 12:03:51 +0000

    thematics page

---
+++ src/app/pages/thematics-page/thematics-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thematics-page',
+  templateUrl: './thematics-page.component.html',
+  styleUrls: ['./thematics-page.component.scss']
+})
+export class ThematicsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
