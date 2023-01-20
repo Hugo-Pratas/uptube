6ad2f67d471ae7897dc990824bb99e144a9c9412
FranciscoLopes01 <cursoupskills@gmail.com>
2023-01-20 18:12:10 +0000

    all channels almost done

--- src/app/pages/all-channels/all-channels.component.ts
+++ src/app/pages/all-channels/all-channels.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
 import {UpTubeServiceService} from "../../services/up-tube-service.service";
-import {faBookmark} from "@fortawesome/free-regular-svg-icons";
+import {Channel} from "../../model/channel";
 
 @Component({
   selector: 'app-all-channels',
@@ -9,22 +8,18 @@ import {faBookmark} from "@fortawesome/free-regular-svg-icons";
   styleUrls: ['./all-channels.component.scss']
 })
 export class AllChannelsComponent implements OnInit {
-  channel: any;
-  id_channel = -1;
-  bookmark = faBookmark;
+  channels= [] as Channel[];
   processedPage = false;
-  apiLink = "";
-
-  constructor(private route: ActivatedRoute, private _service: UpTubeServiceService) {
+  constructor(private _service: UpTubeServiceService) {
   }
 
-  ngOnInit(): void {
-    this.apiLink = this._service.getApiRoute();
-      this._service.getChannels().subscribe(channel => {
-        this.channel = channel
-      })
-      console.log(this.channel)
 
-  }
+    async ngOnInit(): Promise<void> {
+      this.channels = await this._service.getChannels()
+      this.processedPage = true
+    }
+
+
+
 
 }
